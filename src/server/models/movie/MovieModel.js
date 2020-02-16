5325744bbe697475631cb7f9f193f81ffabd77a5
Neal Fennimore <hi@neal.codes>
2020-02-16 00:57:47 -0500

    Add in ID to MovieModel

--- src/server/models/movie/MovieModel.js
+++ src/server/models/movie/MovieModel.js
@@ -1,12 +1,14 @@
 /* eslint-disable camelcase */
 class MovieModel {
     constructor({
+        id,
         title,
         format,
         movie_length,
         release_year,
         rating
     }){
+        this.id = id;
         this.title = title;
         this.format = format;
         this.movie_length = movie_length;
