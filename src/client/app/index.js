6aa7f74b658d5f6b1bd23c23134aa1cec5081b4e
Neal Fennimore <hi@neal.codes>
2020-02-18 16:34:42 -0500

    Add in react app

---
+++ src/client/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = (props) =>{
+    return (
+        <main>App</main>
+    );
+};
+
+App.propTypes ={};
+
+export default App;
--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,17 @@
-// TODO
+/**
+ * External Dependencies
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Internal Dependencies
+ */
+import App from './app';
+
+ReactDOM.render(
+    (
+        <App />
+    ),
+    document.body
+);
