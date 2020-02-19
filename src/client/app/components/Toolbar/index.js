c78f137b9370339722d9493afed64cb9c8d3606d
Neal Fennimore <hi@neal.codes>
2020-02-18 21:00:13 -0500

    Add in Drawer component

--- src/client/app/components/Toolbar/index.js
+++ src/client/app/components/Toolbar/index.js
@@ -3,30 +3,43 @@
  */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import CameraRollIcon from '@material-ui/icons/CameraRoll';
+import { makeStyles } from '@material-ui/core/styles';
+
+/**
+ * Internal Dependencies
+ */
+import Drawer from '../Drawer';
+
+/**
+ * Styles for component
+ */
+const useStyles = makeStyles( theme => ( {
+    toolbar: {
+        justifyContent: 'space-between',
+    },
+} ) );
 
 const AppToolbar = ( props ) => {
     const [isOpen, setOpen] = useState( false );
     const toggle = () => setOpen( ! isOpen );
+    const classes = useStyles();
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggle}>
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6">
-                    movies.neal.cloud
-                </Typography>
-                <Menu
-                    id="menu"
-                    open={isOpen}
-                    onClose={toggle}
-                >
-                    <MenuItem onClick={toggle}>Home</MenuItem>
-                </Menu>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="static">
+                <Toolbar className={classes.toolbar}>
+                    <IconButton color="inherit">
+                        <CameraRollIcon />
+                    </IconButton>
+                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggle}>
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer isOpen={isOpen} onClose={toggle} />
+        </>
     );
 };
 
