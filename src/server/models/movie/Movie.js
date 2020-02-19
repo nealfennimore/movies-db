5747841b3020c67f92b25b889180eeaa4470bf09
Neal Fennimore <hi@neal.codes>
2020-02-18 21:07:45 -0500

    Fix linter errors

--- src/server/models/movie/Movie.js
+++ src/server/models/movie/Movie.js
@@ -2,8 +2,8 @@
 /**
  * Internal Dependencies
  */
-const MovieModel = require('./MovieModel');
-const modelValidator = require('server/models/validator');
+const MovieModel = require( './MovieModel' );
+const modelValidator = require( 'server/models/validator' );
 
 /**
  * Movie
@@ -17,14 +17,14 @@ class Movie {
      * @static
      * @memberof Movie
      */
-    static validator = modelValidator({
-        id: value => typeof Number(value) === 'number' && value < Infinity,
+    static validator = modelValidator( {
+        id: value => typeof Number( value ) === 'number' && value < Infinity,
         title: value => typeof value === 'string' && value.length >= 1 && value.length <= 50,
-        format: value => typeof value === 'string' && ['VHS', 'DVD', 'Streaming'].includes(value),
+        format: value => typeof value === 'string' && ['VHS', 'DVD', 'Streaming'].includes( value ),
         movie_length: value => typeof value === 'number' && value >= 0 && value <= 500,
         release_year: value => typeof value === 'number' && value >= 1800 && value <= 2100,
         rating: value => typeof value === 'number' && value >= 1 && value <= 5,
-    });
+    } );
 
     /**
      * Create a movie instance
@@ -36,14 +36,14 @@ class Movie {
      * @throws {TypeError} When invalid constructor args used
      * @memberof Movie
      */
-    static create( constructorArgs={}, ...args){
-        const ConstructorProxy = new Proxy(MovieModel, Movie.validator);
-    
+    static create( constructorArgs = {}, ...args ) {
+        const ConstructorProxy = new Proxy( MovieModel, Movie.validator );
+
         // Create movie instance by running it through constructor validity handler
-        const movie = new ConstructorProxy(constructorArgs, ...args);
+        const movie = new ConstructorProxy( constructorArgs, ...args );
 
         // Return new proxy with movie instance to use setter validity handlers correctly
-        return new Proxy(movie, Movie.validator);
+        return new Proxy( movie, Movie.validator );
     }
 }
 
