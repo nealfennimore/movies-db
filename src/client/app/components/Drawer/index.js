5a30dccbe8e68bfbf9bf9ab4561a3aa81f632d94
Neal Fennimore <hi@neal.codes>
2020-02-20 15:30:35 -0500

    Update Drawer styles, text, and functionality

--- src/client/app/components/Drawer/index.js
+++ src/client/app/components/Drawer/index.js
@@ -3,29 +3,56 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Drawer, List, ListItem } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { Drawer, List, ListItem, ListItemIcon } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles( theme => ( {
+    list: {
+        width: 250,
+    },
+    link: {
+        color: theme.palette.text.primary,
+        textDecoration: 'none',
+        ...theme.typography.button,
+    },
+} ) );
+
 
 const AppDrawer = ( {
     isOpen,
     onClose,
-} ) => (
-    <Drawer anchor="right" open={isOpen} onClose={onClose}>
-        <List>
-            <ListItem>
-                <Link to="/">
-                    Home
-                </Link>
-            </ListItem>
-            <ListItem>
-                <Link to="/">
-                    Create Movie
-                </Link>
-            </ListItem>
-        </List>
-    </Drawer>
-);
+} ) => {
+    const classes = useStyles();
+    return (
+        <Drawer anchor="right" open={isOpen} onClose={onClose}>
+            <List className={classes.list}>
+                <ListItem>
+                    <ListItemIcon>
+                        <HomeIcon />
+                    </ListItemIcon>
+                    <NavLink className={classes.link} onClick={onClose} to="/">
+                        Home
+                    </NavLink>
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon>
+                        <AddIcon />
+                    </ListItemIcon>
+                    <NavLink  className={classes.link} onClick={onClose} to="/movies/new">
+                        Create Movie
+                    </NavLink>
+                </ListItem>
+            </List>
+        </Drawer>
+    );
+};
 
-AppDrawer.propTypes = {};
+AppDrawer.propTypes = {
+    isOpen: PropTypes.bool,
+    onClose: PropTypes.func,
+};
 
 export default AppDrawer;
