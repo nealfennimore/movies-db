bd4c87d8c1a7866c13d5092cf1027b65e73bc580
Neal Fennimore <hi@neal.codes>
2020-02-25 09:59:34 -0500

    Minor cleaup

--- src/server/database/migrations/index.js
+++ src/server/database/migrations/index.js
@@ -4,10 +4,10 @@ async function migrations( db ) {
         CREATE TABLE IF NOT EXISTS movies (
             id SERIAL PRIMARY KEY,
             title VARCHAR(50) NOT NULL CHECK(length(title) >= 1),
-            format VARCHAR(20) not null check(format in ('VHS', 'DVD', 'Streaming')),
+            format VARCHAR(20) NOT NULL CHECK(format in ('VHS', 'DVD', 'Streaming')),
             movie_length SMALLINT NOT NULL CHECK(movie_length >= 0 AND movie_length <= 500),
             release_year SMALLINT NOT NULL CHECK(release_year >= 1800 AND release_year <= 2100),
-            rating smallint NOT NULL CHECK(rating >= 1 AND rating <= 5)
+            rating SMALLINT NOT NULL CHECK(rating >= 1 AND rating <= 5)
         );
     ` );
 }
